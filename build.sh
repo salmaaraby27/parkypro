e37240a1c0209ad476afae953b5b4892af811af5
salmaaraby27 <124284337+salmaaraby27@users.noreply.github.com>
2025-06-21 22:27:05 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,24 @@
+# #!/usr/bin/env bash
+
+# set -o errexit
+
+
+# pip install -r requirements.txt
+
+# python manage.py collectstatic --no-input
+# python manage.py migrate
+
 #!/usr/bin/env bash
 
+# أوقف السكريبت لو فيه أي خطأ
 set -o errexit
 
-
+# تثبيت المتطلبات
+pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# ترحيل قاعدة البيانات
 python manage.py migrate
+
+# جمع ملفات static
+python manage.py collectstatic --no-input
