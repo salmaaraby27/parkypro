71243520c2010c1fdac2757cd7e6f0e27305365f
salmaaraby27 <124284337+salmaaraby27@users.noreply.github.com>
2025-06-21 22:48:45 +0300

    Update urls.py

--- src/urls.py
+++ src/urls.py
@@ -18,10 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
-from .views import redirect_to_api
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', redirect_to_api),
+   path('', RedirectView.as_view(url='/api/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', include('parking.urls')),
 ]
